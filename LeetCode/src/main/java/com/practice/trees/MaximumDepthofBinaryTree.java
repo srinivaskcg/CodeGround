74b87fcb38d8fed3c878e83df31d6972c445fbd6
Gubbala <sgubbala@informatica.com>
2016-08-11 15:22:35 -0700

    directory changes

---
+++ LeetCode/src/main/java/com/practice/trees/MaximumDepthofBinaryTree.java
@@ -0,0 +1,12 @@
+package com.practice.trees;
+
+public class MaximumDepthofBinaryTree {
+	public int maxDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+	}
+}
+
+
---
+++ LeetCode/src/main/java/com/practice/trees/TreeNode.java
@@ -0,0 +1,11 @@
+package com.practice.trees;
+
+public class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
